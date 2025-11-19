30e5f21d6af4623bbaab5b31d86b80a8470646bc
ZahirRodriguez <rodriguez.mora.zahir.15@gmail.com>
2025-11-19 01:31:30 -0600

    chore(frontend): eliminar módulo de finanzas, builders y limpiar env

--- frontend/smart-home-pwa/src/environments/environment.prod.ts
+++ frontend/smart-home-pwa/src/environments/environment.prod.ts
@@ -7,7 +7,7 @@ export const environment = {
     notifications: 'https://api.smarthome.com/api/notifications',
     fileUpload: 'https://api.smarthome.com/api/files',
     aiQuery: 'https://api.smarthome.com/api',
-    finance: 'https://api.smarthome.com/api/finance'
+    
   },
   notificationsSocketUrl: 'https://api.smarthome.com'
 };
